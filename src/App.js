f61ff9bae732bc48156a1a367054431ade0f4a61
mehedi-cryptic <dexterousmehedi977@gmail.com>
2022-11-11 12:36:07 +0600

    api using single user , multi users and random api user

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 //import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
 
 
@@ -22,12 +22,40 @@ function App() {
 
   }
 
+const [users, setUsers]=useState([]);
+useEffect(()=> {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res=>res.json())
+  .then(data=> setUsers(data))
+},[])
+
+const[singleUser, setSingleUser]=useState({});
+const [randomUser, setRandomUser]=useState({});
+useEffect(()=> {
+  fetch('https://jsonplaceholder.typicode.com/users/1')
+  .then(res=>res.json())
+  .then(data=>setSingleUser(data));
+
+
+  fetch('https://randomuser.me/api')
+  .then(res=>res.json())
+  .then(data=>setRandomUser(data.results[0]))
+},[])
 
   return (
     <div className="App">
       <AccessAlarmIcon style={{border: '1px solid black' , margin:'10px', borderRadius:'10px' , padding:'10px'}} onClick={handleAlarm} color={alarmColor} ></AccessAlarmIcon>
 
       <ThumbUpAltIcon style={{border: '1px solid black' , margin:'10px', borderRadius:'10px' , padding:'10px'}} onClick={handleLike} color={likeColor}> </ThumbUpAltIcon>
+      <h2>
+        RandomUser:   {randomUser.name && randomUser.name.first}
+      </h2>
+
+
+     {
+      users.map( user=> <li> {user.name}</li>)
+     }
+     <h1>{singleUser.name}</h1>
       
     </div>
   );
